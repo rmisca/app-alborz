b69c1892a2b5a6f5918e11adb3b31517f3f7d594
rmisca <rominamisca95@gmail.com>
2019-11-07 23:22:09 +0200

    add season dropdown selection

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-var name = "Romina";
-function show(name223, age){
-    console.log (name223, age);   
+document.querySelector('.custom-select-wrapper').addEventListener('click', function() {
+    this.querySelector('.custom-select').classList.toggle('open');
+})
+for (const option of document.querySelectorAll(".custom-option")) {
+    option.addEventListener('click', function() {
+        if (!this.classList.contains('selected')) {
+            this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
+            this.classList.add('selected');
+            this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+        }
+    })
 }
-show(name)
+window.addEventListener('click', function(e) {
+    const select = document.querySelector('.custom-select')
+    if (!select.contains(e.target)) {
+        select.classList.remove('open');
+    }
+});
