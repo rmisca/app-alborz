6a600821947c34baf4d7959b2308fc34f0f8d01e
rmisca <rominamisca95@gmail.com>
2019-11-21 21:54:17 +0200

    finish first team table

--- data/teams.js
+++ data/teams.js
@@ -1,37 +1,41 @@
 var data = {
     "teams": [
         {
+            "position": 1,
             "name": "ON TIME CONSTRUCT",
             "games": 0,
             "wins": 0,
             "draws": 0,
             "defeats": 0,
             "goalsScored": 0,
-            "goalsreceived": 0,
-            "points": 0
+            "goalsReceived": 0,
+            "points": 0,
+            "ligue": 3
         },
         {
+            "position": 2,
             "name": "SEVEN CLUB",
             "games": 0,
             "wins": 0,
             "draws": 0,
             "defeats": 0,
             "goalsScored": 0,
-            "goalsreceived": 0,
+            "goalsReceived": 0,
             "points": 0
         },
         {
+            "position": 3,
             "name": "SCORPIONIII",
             "games": 0,
             "wins": 0,
             "draws": 0,
             "defeats": 0,
             "goalsScored": 0,
-            "goalsreceived": 0,
+            "goalsReceived": 0,
             "points": 0
         }
     ],
     "headers": [
-        "Numar", "Echipa", "M", "V", "I", "GM", "GP", "PCT"
+        "Pozitie", "Echipa", "M", "V", "E", "I", "GM", "GP", "PCT"
     ]
 }
--- js/work.js
+++ js/work.js
@@ -21,18 +21,47 @@
                 html += "<th>";
             }
         }
-        html += "</tr></thead></table>";
+        html += "</tr></thead>";
         return html;
     }
     function buildTeamTableBody(data) {
-        //todo Romina to create table body
+        var body = data.teams;
+        var html = "<tbody><tr><td>";
+        for (var i = 0; i < body.length; i++) {
+            html += body[i]["position"] + "</td><td>";
+            html += body[i]["name"] + "</td><td>";            
+            html += body[i]["games"] + "</td><td>";            
+            html += body[i]["wins"] + "</td><td>";            
+            html += body[i]["draws"] + "</td><td>";            
+            html += body[i]["defeats"] + "</td><td>";            
+            html += body[i]["goalsScored"] + "</td><td>";            
+            html += body[i]["goalsReceived"] + "</td><td>";            
+            html += body[i]["points"] + "</td></tr>"; 
+            if (i < body.length - 1) {
+                html += "<tr><td>";
+            }
+        }
+        html += "</tbody></table>";
+        return html;
     }
     function buildTeamTable(header, body){
         //todo :(
         var jsonData = data;
-        var header = buildTeamTableHeader(jsonData);
+        var header = buildTeamTableHeader(jsonData);//"html"
+        var body = buildTeamTableBody(jsonData);
+        var table = header + body;
         var container = document.getElementById("dynamic-table");
-        container.innerHTML = header;
+        container.innerHTML = table;
+
     }
     buildTeamTable();
+
+    var btns = document.getElementsByClassName("mr-click-event");
+  
+    function show (event) {
+        console.log(event);
+    }
+    for (var j = 0; j < btns.length; j++) {
+        btns[j].addEventListener ("click", show)
+    }
 })()
